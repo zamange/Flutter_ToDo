445aa8408733572b23374e4f35f87eb0bd0baa91
WTC <zngeyane023@student.wethinkcode.co.za>
2024-11-22 10:31:35 +0200

    added constructors

--- to_do/lib/home_page.dart
+++ to_do/lib/home_page.dart
@@ -1,14 +1,20 @@
+// home_page.dart
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key); // Constructor added
 
-    @override
-    State<HomePage> createState() => _HomePageState();
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome to the To-Do App!'), // Added content for clarity
+      ),
+    );
+  }
 }
--- to_do/lib/main.dart
+++ to_do/lib/main.dart
@@ -1,14 +1,20 @@
+// main.dart
 import 'package:flutter/material.dart';
-import 'package:to_do/home_page.dart';
-
+import 'package:to_do/home_page.dart'; // Correct import path
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  return const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  );
+  const MyApp({Key? key}) : super(key: key); // Constructor added
+
+  @override
+  Widget build(BuildContext context) {
+    // Build method for the widget tree
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
 }
